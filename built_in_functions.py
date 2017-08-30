69d6d024bbc3642a944474dc5712681a1415f084
KaustubhDhokte <kaustubh.dhokte@gmail.com>
2017-08-30 09:36:55 +0530

    enlist built in functions to be studied

---
+++ built_in_functions.py
@@ -0,0 +1,125 @@
+#http://www.bogotobogo.com/python/python_functions_built_in.php
+#https://docs.python.org/2/library/functions.html
+
+'''
+TO DO: Study following built-in functions
+
+
+abs(x)
+Return the absolute value of a number. 
+The argument may be an integer or a floating point number. 
+If the argument is a complex number, its magnitude is returned.
+
+all(iterable)
+Return True if all elements of the iterable are true (or if the iterable is empty).
+
+any(iterable)
+Return True if any element of the iterable is true. If the iterable is empty, return False.
+
+class bool([x])
+Return a Boolean value, i.e. one of True or False. 
+x is converted using the standard truth testing procedure. 
+If x is false or omitted, this returns False; otherwise it returns True. 
+The bool class is a subclass of int (see Numeric Types — int, float, complex). 
+It cannot be subclassed further. Its only instances are False and True (see Boolean Values).
+
+bytearray 
+
+bytes
+
+callable
+
+classmethod
+
+compile
+
+complex
+
+delattr
+
+dir
+
+divmod 
+
+enumerate 
+
+eval 
+
+exec 
+
+filter 
+
+float 
+
+format 
+
+frozenset 
+
+getattr 
+
+globals 
+
+hasattr 
+
+id  
+
+input 
+
+isinstance 
+
+issubclass 
+
+iter 
+
+len 
+
+locals 
+
+map 
+
+max 
+
+memoryview 
+
+min 
+
+next 
+
+open 
+
+pow 
+
+property 
+
+range 
+
+repr 
+
+reversed  
+
+set 
+
+setattr 
+
+slice 
+
+sorted 
+
+staticmethod 
+
+str 
+
+sum 
+
+super 
+
+tuple 
+
+type 
+
+vars 
+
+zip 
+
+__import__
+'''
