b2983f69e04af6b779351ee90af7cacef8e24307
Kaustubh <KaustubhDhokte@users.noreply.github.com>
2017-11-09 12:35:50 +0530

    Descriptors added with TODO
    Complete the TODO exercise.

---
+++ descriptors_TODO.py
@@ -0,0 +1,3 @@
+# https://www.ibm.com/developerworks/library/os-pythondescriptors/index.html
+# https://www.smallsurething.com/python-descriptors-made-simple/
+# https://www.blog.pythonlibrary.org/2016/06/10/python-201-what-are-descriptors/
